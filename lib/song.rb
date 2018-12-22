cf02a70937fdd936a8aa1de0f9f80dadd588a481
Nora Vogt <noraevogt@gmail.com>
2018-12-22 19:05:42 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,3 +1,4 @@
+require 'pry'
 class Song
   attr_accessor :name, :artist_name
   @@all = []
@@ -10,4 +11,59 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    song
+  end
+
+  def self.create_by_name(song_name)
+    song = self.new
+    song.name = song_name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(song_name)
+    self.all.find {|song| song.name == song_name}
+  end
+
+  def self.find_or_create_by_name(song_name)
+    # if the song instance already exists, return it
+    # if not, create the new song instance and return it
+    if self.find_by_name(song_name)
+      self.find_by_name(song_name)
+    else self.create_by_name(song_name)
+    end
+  end
+
+  def self.alphabetical
+    @@all.sort_by {|a| a.name }
+  end
+
+  def self.new_from_filename(file_name)
+    split_file = file_name.split(/[-.]/)
+    artist = split_file[0].strip
+    song_name = split_file[1].strip
+
+    song = Song.new
+    song.name = song_name
+    song.artist_name = artist
+
+    song
+  end
+
+  def self.create_from_filename(file_name)
+    @@all << self.new_from_filename(file_name)
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
 end
